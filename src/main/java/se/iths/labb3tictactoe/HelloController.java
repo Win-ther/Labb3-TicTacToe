623e4023e4988155e6dcf3949719482df0638726
Win-ther <emil.winther@iths.se>
2023-10-17 19:48:57 +0200

    added som stuff, kommer inte igåg är  för ftrött

--- src/main/java/se/iths/labb3tictactoe/HelloController.java
+++ src/main/java/se/iths/labb3tictactoe/HelloController.java
@@ -3,14 +3,17 @@ package se.iths.labb3tictactoe;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class HelloController {
@@ -25,16 +28,15 @@ public class HelloController {
     @FXML
     private GridPane grid;
     @FXML
-    private Text winnerText;
-    private int turn = 0;
-
-    public Label welcomeText;
+    private Label winnerText;
     private Model model = new Model();
-
+    private GraphicsContext gc;
+    @FXML
+    private Canvas canvas1, canvas2, canvas3, canvas4, canvas5, canvas6, canvas7, canvas8, canvas9;
     @FXML
     protected void onButtonClick(MouseEvent mouseEvent) {
-        if (mouseEvent.getX() < grid.getWidth())
-            grid.add(new Text("X"), 0, 0);
+        if(mouseEvent.)
+
     }
 
     public Model getModel() {
@@ -42,6 +44,24 @@ public class HelloController {
     }
 
     public void initialize() {
-        welcomeText.textProperty().bind(model.textProperty());
+        winnerText.textProperty().bind(model.textProperty());
+        List<Canvas> canvasList = List.of(canvas1, canvas2, canvas3, canvas4, canvas5, canvas6, canvas7, canvas8, canvas9);
+        //canvasList.forEach(e -> primeSquares);
+    }
+
+    private void setSquare(Canvas canvas){
+        gc = canvas.getGraphicsContext2D();
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(2);
+        gc.strokeLine(0, 0, canvas.getWidth(), canvas.getHeight());
+        gc.strokeLine(canvas.getWidth(), 0, 0, canvas.getHeight());
+
+
+    }
+    private void setOval(Canvas canvas){
+        gc = canvas.getGraphicsContext2D();
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(2);
+        gc.strokeOval(0, 0, canvas.getWidth(), canvas.getHeight());
     }
 }
--- src/main/java/se/iths/labb3tictactoe/Model.java
+++ src/main/java/se/iths/labb3tictactoe/Model.java
@@ -1,13 +1,45 @@
 package se.iths.labb3tictactoe;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.GridPane;
 
 public class Model {
-private StringProperty text = new SimpleStringProperty();
-
+    private StringProperty text = new SimpleStringProperty();
+    private IntegerProperty turn = new SimpleIntegerProperty();
+    private String[][] grid;
 
     public Model() {
+        text.setValue("TIC TAC TOE");
+        turn.setValue(0);
+        grid = new String[3][3];
+    }
+
+    public String[][] getGrid() {
+        return grid;
+    }
+
+    public void setSymbol(Canvas canvas) {
+        if (turn.get() == 0){
+        }
+    }
+
+    public int getTurn() {
+        return turn.get();
+    }
+
+    public IntegerProperty turnProperty() {
+        return turn;
+    }
+
+    public void changeTurn() {
+        if (turn.get() == 0)
+            turn.set(1);
+        else
+            turn.set(0);
     }
 
     public String getText() {
