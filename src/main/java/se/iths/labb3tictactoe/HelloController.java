ffa51d150c8d3b220fba4be87328fe600cbecda0
Win-ther <emil.winther@iths.se>
2023-10-17 11:34:56 +0200

    changed layout and added test for print X on screen

--- src/main/java/se/iths/labb3tictactoe/HelloController.java
+++ src/main/java/se/iths/labb3tictactoe/HelloController.java
@@ -1,22 +1,36 @@
 package se.iths.labb3tictactoe;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
-public class HelloController {
-    //Använd Canvas istället för buttons kanske; Kan se bättre ut.
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class HelloController implements Initializable {
+    //Använd Canvas istället för buttons kanske; Kan se bättre ut.
+    //Kontrollera klick med koordinater, MouseEvent mouseEvent
     @FXML
-    private Button button1, button2, button3, button4, button5, button6, button7, button8, button9;
+    Canvas canvas;
     @FXML
     private Text winnerText;
-
+    @FXML
+    private Text text1;
     private int turn = 0;
 
     @FXML
-    protected void onButtonClick() {
+    protected void onButtonClick(MouseEvent mouseEvent) {
+        if (mouseEvent.getX() < canvas.getWidth()/3 && mouseEvent.getY() < canvas.getHeight()/3)
+            text1.setText("X");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
 }
