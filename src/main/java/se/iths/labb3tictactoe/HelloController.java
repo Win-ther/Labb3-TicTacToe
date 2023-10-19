a3ecfda68b8efe5dfa46497aaee00aae696bfcee
Win-ther <emil.winther@iths.se>
2023-10-19 15:19:48 +0200

    Added Ai-logic

--- src/main/java/se/iths/labb3tictactoe/HelloController.java
+++ src/main/java/se/iths/labb3tictactoe/HelloController.java
@@ -10,46 +10,78 @@ import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class HelloController {
     //Controller hanterar event, klickar på skit
-    //Modellen ansvaring för informationen och regler om informationen.
+    //Modellen ansvarig för informationen och regler om informationen.
     //Controller ska där med hjälp av event uppdatera informationen i Modellen.
     //ändra i model endast och bind vyns fält med dem i Model.
     //Går att binda dem även i fxml-filen, se welcomeText;
     //Använd Canvas istället för buttons kanske; Kan se bättre ut.
     //Kontrollera klick med koordinater, MouseEvent mouseEvent
-    //ToDO: Grid ~Done~, logic for clicking squares ~Done~, turnorder logic, GameOver Logic, AI Logic.
-    @FXML
-    private GridPane grid;
-    @FXML
-    private Label winnerText;
+    //ToDO: Grid ~Done~, logic for clicking squares ~Done~, turnorder logic~Done~, GameOver Logic~Done~, AI Logic.
     private Model model = new Model();
-    private GraphicsContext gc;
     @FXML
-    private Button button1, button2, button3, button4, button5, button6, button7, button8, button9, restartButton;
-
+    private Button button1, button2, button3, button4, button5, button6, button7, button8, button9, restartButton,playButton;
     private List<Button> buttons;
+    @FXML
+    private GridPane playArea;
+    @FXML
+    private Text tictictic,tactactac,toetoetoe;
 
     @FXML
     public void restart(ActionEvent event){
         model.reset(buttons);
+        cpuTurn();
     }
 
     @FXML
     private void onButtonClick(ActionEvent event) {
         model.setSymbol((Button) event.getSource());
         model.gameOver(buttons);
+        if (!model.isGameOver())
+            cpuTurn();
     }
 
     public Model getModel() {
         return model;
     }
 
+    public void cpuTurn(){
+        Random random = new Random();
+        int buttonNumber;
+        while (true){
+            buttonNumber = random.nextInt(9);
+            if (usableButton(buttonNumber)){
+                model.setSymbol(buttons.get(buttonNumber));
+                model.gameOver(buttons);
+                break;
+            }
+        }
+    }
+    private boolean usableButton(int index){
+        return !buttons.get(index).isDisabled();
+    }
     public void initialize() {
         buttons = Arrays.asList(button1, button2, button3, button4, button5, button6, button7, button8, button9);
+        buttons.forEach(e -> e.setFocusTraversable(false));
+        playArea.setVisible(false);
+        tictictic.setVisible(false);
+        tactactac.setVisible(false);
+        toetoetoe.setVisible(false);
+    }
+
+    public void startGame(ActionEvent event) {
+        playButton.setVisible(false);
+        playArea.setVisible(true);
+        tictictic.setVisible(true);
+        tactactac.setVisible(true);
+        toetoetoe.setVisible(true);
+        cpuTurn();
     }
 }
--- src/main/java/se/iths/labb3tictactoe/Model.java
+++ src/main/java/se/iths/labb3tictactoe/Model.java
@@ -6,48 +6,48 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.scene.control.Button;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Model {
     private StringProperty winnerText = new SimpleStringProperty();
-    private IntegerProperty turn = new SimpleIntegerProperty();
-    private String[][] grid;
+    private IntegerProperty xoTurn = new SimpleIntegerProperty();
+    private int turnTotal;
+    private boolean isGameOver;
+
 
     public Model() {
         winnerText.setValue("TIC TAC TOE");
-        turn.setValue(0);
-        grid = new String[3][3];
+        xoTurn.setValue(0);
+        turnTotal = 0;
+        isGameOver = false;
     }
 
-    public String[][] getGrid() {
-        return grid;
-    }
 
     public void setSymbol(Button button) {
-        if (turn.get() == 0){
+        if (xoTurn.get() == 0) {
             button.setText("X");
-            button.setDisable(true);
-            nextTurn();
-        }else{
+        } else {
             button.setText("0");
-            button.setDisable(true);
-            nextTurn();
         }
+        button.setDisable(true);
+        nextTurn();
     }
 
-    public int getTurn() {
-        return turn.get();
+    public int getXoTurn() {
+        return xoTurn.get();
     }
 
-    public IntegerProperty turnProperty() {
-        return turn;
+    public IntegerProperty xoTurnProperty() {
+        return xoTurn;
     }
 
     public void nextTurn() {
-        if (turn.get() == 0)
-            turn.set(1);
+        if (xoTurn.get() == 0)
+            xoTurn.set(1);
         else
-            turn.set(0);
+            xoTurn.set(0);
+        turnTotal++;
     }
 
     public String getWinnerText() {
@@ -63,37 +63,62 @@ public class Model {
     }
 
     public void gameOver(List<Button> buttons) {
-        String winningLine = "";
-        for (int i = 0; i < 8; i++) {
-            winningLine = switch (i){
-                case 0 -> buttons.get(0).getText() + buttons.get(1).getText() + buttons.get(2).getText();
-                case 1 -> buttons.get(3).getText() + buttons.get(4).getText() + buttons.get(5).getText();
-                case 2 -> buttons.get(6).getText() + buttons.get(7).getText() + buttons.get(8).getText();
-                case 3 -> buttons.get(0).getText() + buttons.get(3).getText() + buttons.get(6).getText();
-                case 4 -> buttons.get(1).getText() + buttons.get(4).getText() + buttons.get(7).getText();
-                case 5 -> buttons.get(2).getText() + buttons.get(5).getText() + buttons.get(8).getText();
-                case 6 -> buttons.get(0).getText() + buttons.get(4).getText() + buttons.get(8).getText();
-                case 7 -> buttons.get(2).getText() + buttons.get(4).getText() + buttons.get(6).getText();
-                default -> "";
-            };
-            if (winningLine.equals("XXX")){
-                setWinnerText("X Won!");
-                buttons.forEach(e -> e.setDisable(true));
-            } else if (winningLine.equals("000")) {
-                setWinnerText("0 Won!");
-                buttons.forEach(e -> e.setDisable(true));
-            }
+        String[] winningLines = {
+                buttons.get(0).getText() + buttons.get(1).getText() + buttons.get(2).getText(),
+                buttons.get(3).getText() + buttons.get(4).getText() + buttons.get(5).getText(),
+                buttons.get(6).getText() + buttons.get(7).getText() + buttons.get(8).getText(),
+                buttons.get(0).getText() + buttons.get(3).getText() + buttons.get(6).getText(),
+                buttons.get(1).getText() + buttons.get(4).getText() + buttons.get(7).getText(),
+                buttons.get(2).getText() + buttons.get(5).getText() + buttons.get(8).getText(),
+                buttons.get(0).getText() + buttons.get(4).getText() + buttons.get(8).getText(),
+                buttons.get(2).getText() + buttons.get(4).getText() + buttons.get(6).getText()
+        };
+        String winningLine = Arrays.stream(winningLines).filter(w -> w.equals("XXX") || w.equals("000")).findFirst().orElse("");
+        if (winningLine.equals("XXX")) {
+            setWinnerText("X Won!");
+            disableButtons(buttons);
+        } else if (winningLine.equals("000")) {
+            setWinnerText("0 Won!");
+            disableButtons(buttons);
+        }else if(turnTotal > 8){
+            setWinnerText("Draw");
+            disableButtons(buttons);
         }
     }
+
+    private void disableButtons(List<Button> buttons) {
+        buttons.forEach(e -> e.setDisable(true));
+        this.isGameOver = true;
+    }
+
+
 
     public void reset(List<Button> buttons) {
         this.winnerText.set("TIC TAC TOE");
         buttons.forEach(this::resetButton);
+        this.turnTotal = 0;
+        this.isGameOver = false;
     }
 
-    private void resetButton(Button e) {
-        e.setText("");
-        e.setDisable(false);
-        this.turn.set(0);
+    private void resetButton(Button button) {
+        button.setText("");
+        button.setDisable(false);
+        this.xoTurn.set(0);
+    }
+
+    public int getTurnTotal() {
+        return turnTotal;
+    }
+
+    public void setTurnTotal(int turnTotal) {
+        this.turnTotal = turnTotal;
+    }
+
+    public boolean isGameOver() {
+        return isGameOver;
+    }
+
+    public void setGameOver(boolean gameOver) {
+        isGameOver = gameOver;
     }
 }
